7ccb0830f4332ae2a4cebec864e0160c3531b0c7
Matt Goldman <matt.goldman@outlook.com>
2022-09-03 10:28:54 +1000

    Label animation working

--- MauiMaterialEntry/Controls/MaterialEntry.xaml.cs
+++ MauiMaterialEntry/Controls/MaterialEntry.xaml.cs
@@ -9,12 +9,26 @@ public partial class MaterialEntry : ContentView
 
 	private void MEEntry_Focused(object sender, FocusEventArgs e)
 	{
-		MELabel.IsVisible = false;
+		//MELabel.IsVisible = false;
+		ScaleLabelDown();
     }
 
 	private void MEEntry_Unfocused(object sender, FocusEventArgs e)
 	{
 		if (string.IsNullOrWhiteSpace(MEEntry.Text))
-			MELabel.IsVisible = true;
+			//MELabel.IsVisible = true;
+			ScaleLabelUp();
     }
+
+	private void ScaleLabelDown()
+	{
+		MELabel.ScaleTo(0.8, 250, Easing.Linear);
+		MELabel.TranslateTo(-50, -20, 250, Easing.Linear);
+	}
+
+	private void ScaleLabelUp()
+	{
+		MELabel.ScaleTo(1, 250, Easing.Linear);
+		MELabel.TranslateTo(0, 0, 250, Easing.Linear);
+	}
 }
